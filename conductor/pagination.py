5ee68a0f0f24ce9ddf1c6309cb8489ede952ee02
LioQing <s2010157@gmail.com>
2023-12-03 13:48:44 +0800

    Improve pagination

--- conductor/pagination.py
+++ conductor/pagination.py
@@ -1,4 +1,4 @@
-from rest_framework import pagination, response
+from rest_framework import exceptions, pagination, response
 
 
 class ChatPagination(pagination.PageNumberPagination):
@@ -11,3 +11,12 @@ class ChatPagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
         """Return the paginated response"""
         return response.Response(data=data)
+
+    def paginate_queryset(self, queryset, request, view=None):
+        """Paginate the queryset"""
+        try:
+            return super(ChatPagination, self).paginate_queryset(
+                queryset, request, view=view
+            )
+        except exceptions.NotFound:
+            return list()
